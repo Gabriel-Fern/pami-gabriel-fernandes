b04e9868ab9d7642749f6960cb71ff5fd1c367f7
Gabriel Fernandes <saygabrielfernandes3105@gmail.com>
2025-02-13 08:32:30 -0300

    Cria o arquivo console.js da aula 01

---
+++ aula 01/console.js
@@ -0,0 +1,9 @@
+console.log("a vida pode ser dura mas nao tao dura quanto o meu professor de fisica do ano passado quando via uma menina do curso de nutriçao");
+
+var A = "oi"
+let B = "Tudo bem"
+const C = 3
+const D = 5968
+console.log(A, B )
+console.log( A + B)
+console.log(C + D)
